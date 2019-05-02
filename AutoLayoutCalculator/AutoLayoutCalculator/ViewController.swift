8bf249c98b879f54b4419a7401b6bf162cb7d919
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2019-05-02 15:48:03 -0500

    Add stack to buttons.

---
+++ AutoLayoutCalculator/AutoLayoutCalculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AutoLayoutCalculator
+//
+//  Created by Álvaro Ávalos Hernández on 5/2/19.
+//  Copyright © 2019 Álvaro Ávalos Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+}
+
